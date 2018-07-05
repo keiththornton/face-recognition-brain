89cc05ab71792ff27f8094378cfab115f63d75f1
Keith Thornton <kathornton2@gmail.com>
2018-07-05 14:37:00 -0500

    Finished Front End

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
-  return(
+  return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}} alt='Logo' src='https://png.icons8.com/metro/1600/brain.png'/> </div>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+        </div>
       </Tilt>
     </div>
   );
